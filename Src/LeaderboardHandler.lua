760c590c177049b8d89e7c5dcbf4d39c5c754a10
CarbonAPI <ryanalex17@sbcglobal.net>
2016-04-12 18:22:45 -0500

    Removed Comments

--- Src/LeaderboardHandler.lua
+++ Src/LeaderboardHandler.lua
@@ -19,13 +19,13 @@ end
 
 coroutine.resume(coroutine.create(function()
 	function joining()
-		local pages = ds:GetSortedAsync(false, 7) 							-- Get all data from ordered data store as Pages object
+		local pages = ds:GetSortedAsync(false, 7) 							
 		local currentPageNumber = 0
 		while true do	
 			if currentPageNumber < 1 then
 				print("Page: " .. currentPageNumber)
-				local data = pages:GetCurrentPage()              				-- Assigning 'data' variable to the current page of Pages object
-				for i, pair in ipairs(data) do 			             				-- Iterates through all key value pairs in 
+				local data = pages:GetCurrentPage()              				
+				for i, pair in ipairs(data) do 			             				
 					print(pair.key .. ": " .. pair.value .. " points, with rank #: " .. i)
 					local startPos = .15
 					local size = .1
@@ -33,10 +33,10 @@ coroutine.resume(coroutine.create(function()
 					local y = startPos + ((i - 1) * size) + ((i - 1) * space)
 					createGui(y, pair.key, pair.value)
 				end
-				if pages.IsFinished then										-- Checks if last page has been reached
+				if pages.IsFinished then										
 					break
 				end
-				pages:AdvanceToNextPageAsync()									-- Iterates to next page
+				pages:AdvanceToNextPageAsync()									
 				currentPageNumber = currentPageNumber + 1
 			else
 				break
